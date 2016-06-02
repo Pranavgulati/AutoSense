64a4145ab020598ba3ca506006adc441a351ffa6
Pranavgulati <pranavgulati95@yahoo.com>
2016-06-02 19:03:29 +0530

    Cheapest Smart Car Sensor Broadcaster
    This is an example use case of the RF24BLE library for the nrf24L01+ to
    make the cheapest possible wireless sensor data broadcaster.

---
+++ AutoSense.ino
@@ -0,0 +1,80 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <RF24BLE.h>
+
+#define PIN_CE  9 // chip enable
+#define PIN_CS  10   // chip select (for SPI)
+// The MAC address of the beacon
+//anything goes but should be constant over time
+#define MY_MAC_0  'A'
+#define MY_MAC_1  'A'
+#define MY_MAC_2  'A'
+#define MY_MAC_3  'A'
+#define MY_MAC_4  'A'
+#define MY_MAC_5  'B'
+#define ANDROID    0x42
+#define IPHONE    0x40
+
+#define ACCZ_PIN A0
+#define MAX_SAMPLES 10
+#define SWEET 0
+#define OKAYY 1
+#define BUMPY 2
+#define GD_ROLLERCOASTER 3 //GD = GOD DAMN 
+#define CAR_NO "DL4CAH6942"
+//MAX_SAMPLES samples per measurement 
+uint8_t accZ[MAX_SAMPLES]={};
+uint8_t RIDE=0;
+RF24 radio(PIN_CE, PIN_CS);
+RF24BLE BLE(radio);
+
+void getSamples(){
+  for(byte i=0;i<MAX_SAMPLES;i++){
+      accZ[i]=analogRead(ACCZ_PIN);
+      delay(1);//makes sure each reading is roughly 1ms apart
+      }
+  }
+void howsDaRide(){
+  //used to check accelerometer's zaxis values and
+  //assign relevant ride characteristics
+  getSamples();
+  int diff=0;
+  
+  for(byte i=0;i+1<MAX_SAMPLES;i++){
+      //find maximum difference here 
+      if(abs(accZ[i+1]-accZ[i])>diff){diff=abs(accZ[i+1]-accZ[i]);}
+      }
+      if(diff<=20){
+          RIDE =SWEET;
+          }
+      else if(diff>20&&diff<=40){
+          RIDE =OKAYY;
+          }
+      else if(diff>40&&diff<=150){
+          RIDE =BUMPY;
+          }
+      else if(diff>150){
+          RIDE =GD_ROLLERCOASTER;
+          }
+    
+  }
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();
+  radio.begin();
+  BLE.begin();
+}
+void loop() {
+  howsDaRide();
+   for (uint8_t channel = 0; channel < 3; channel++){  // Channel hopping do not alter
+    BLE.setPhone(ANDROID);
+    BLE.setMAC(MY_MAC_0, MY_MAC_1, MY_MAC_2, MY_MAC_3, MY_MAC_4, MY_MAC_5);
+    BLE.setName(CAR_NO);
+    BLE.setData(&RIDE, sizeof(RIDE));
+    BLE.sendADV(channel);
+    delay(1);    // Broadcasting interval
+  }
+  delay(500);
+
+}
